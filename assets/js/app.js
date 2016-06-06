8f4334c5840e23ed326db386366138fe6f271b9d
TED <tedlade.tl@gmail.com>
2016-06-06 15:15:17 +0100

    Search functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,32 +5,45 @@ angular.module('app', [
     'restangular',
     'ui.bootstrap',
     'app.controllers'
-])
+    ])
 
 .config(['$stateProvider', '$urlRouterProvider', 'RestangularProvider',
   function($stateProvider, $urlRouterProvider, RestangularProvider) {
-  RestangularProvider.setBaseUrl('https://sahara-health-api.herokuapp.com/');
+      RestangularProvider.setBaseUrl('https://sahara-health-api.herokuapp.com/');
 
-  RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
-      if (data.response && data.response.data) {
-          var returnedData = data.response.data;
-          return returnedData;
-      } else {
-          return data;
-      };
-  });
-  
-  $stateProvider
-    .state('home', {
-    url: '',
-    templateUrl: 'modules/home.html',
-    controller: 'appCtrl'
-  })
-    .state('results', {
-      url: '/bukola-saraki',
-      templateUrl: 'modules/search-result.html',
-      controller: 'resultCtrl'
-    })  
+      RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+          if (data.response && data.response.data) {
+              var returnedData = data.response.data;
+              return returnedData;
+          } else {
+              return data;
+          };
+      });
+      
+      $stateProvider
+      .state('home', {
+        url: '',
+        templateUrl: 'modules/home.html',
+        controller: 'appCtrl'
+    })
+      .state('results', {
+          url: '/search?keyword',
+          templateUrl: 'modules/search-result.html',
+          controller: 'resultCtrl'
+      })  
 
-  $urlRouterProvider.otherwise('/404')  
-}]);
+      $urlRouterProvider.otherwise('/404')  
+  }])
+
+.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        })
+    };
+})
